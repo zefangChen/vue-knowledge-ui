1829df2ef5df51a287f37e5f69eab6eb41a1ffdd
zefangChen <zefang_chen@163.com>
2020-06-10 23:03:11 +0800

    气泡图

--- src/main.js
+++ src/main.js
@@ -7,8 +7,13 @@ import router from './router'
 // import echarts from 'echarts'
 import 'font-awesome/css/font-awesome.css'
 import animated from 'animate.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
+Vue.use(BootstrapVue);     // Install BootstrapVue
+Vue.use(IconsPlugin);      // Optionally install the BootstrapVue icon components plugin
 Vue.config.productionTip = false;
 // Vue.prototype.$echarts = echarts   //全局引入echarts  这种方法是直接绑定在vue实例上，所以在项目中任何页面，直接 this.$echarts 即可
 Vue.use(ElementUI);
--- src/views/excelDownload/index.vue
+++ src/views/excelDownload/index.vue
@@ -3,6 +3,18 @@
         <h5>把excel模板放到前端醒目的static文件夹中，纯前端获取文件下载</h5>
         <a :href="downloadTemplate">下载模板</a>
         <!--<el-button @click="downloadTemplate">下载模板</el-button>-->
+
+
+        <div class="container" style="background-color: #2672ff;width: 100%;">
+            <div class="row">
+                <div class="col-8">
+                    One of three columns
+                </div>
+                <div class="col-4">
+                    One of three columns
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
